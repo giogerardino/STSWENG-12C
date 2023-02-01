67127f5594e838dc4a22bb6f38138edfda27bec3
kimileeee <kim_leejra@dlsu.edu.ph>
2023-02-01 21:54:24 +0800

    added Room implementation in Section

--- src/main/java/com/orangeandbronze/enlistment/Section.java
+++ src/main/java/com/orangeandbronze/enlistment/Section.java
@@ -1,24 +1,27 @@
 package com.orangeandbronze.enlistment;
-import static org.apache.commons.lang3.Validate.notBlank;
 
 import java.util.Objects;
-
+import javax.management.RuntimeErrorException;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.Validate;
 
 class Section{
     private final String sectionID;
     private final Schedule schedule;
+    private final Room room;
+    private int sectionEnlistment = 0;
 
     //class constructor
-    Section(String sectionID, Schedule schedule){
+    Section(String sectionID, Schedule schedule, Room room){
 
-        notBlank(sectionID);
-        Validate.notNull(schedule);
+        Validate.notBlank(sectionID);
         Validate.isTrue(StringUtils.isAlphanumeric(sectionID), "section ID must be AlphaNumeric" + sectionID);
+        Validate.notNull(schedule);
+        Validate.notNull(room);
 
         this.sectionID = sectionID;
         this.schedule = schedule;
+        this.room = room;
     }
 
     boolean hasConflict (Section other) {
@@ -26,11 +29,30 @@ class Section{
     }
 
     void checkForConflict (Section other) {
-        if (this.schedule.equals(other.schedule)) {
+        if (this.schedule.equals(other.schedule) && this.room.equals(other.room)) {
             throw new ScheduleConflictException("this section " + this + 
                 " has the same schedule as other section " + other +
-                " at schedule " + schedule);
+                " at schedule " + schedule + ", " + room);
         }
+    }
+
+    public int getSectionEnlistment() {
+        return sectionEnlistment;
+    }
+
+    public boolean atCapacity() {
+        return sectionEnlistment == room.getCapacity();
+    }
+
+    public void addSectionEnlistment() {
+        sectionEnlistment++;
+    }
+
+    public void subtractSectionEnlistment() {
+        if(sectionEnlistment == 0) {
+            throw new RuntimeErrorException(null, "currentEnlisted cannot be be negative.");
+        }
+        sectionEnlistment--;
     }
 
     @Override
