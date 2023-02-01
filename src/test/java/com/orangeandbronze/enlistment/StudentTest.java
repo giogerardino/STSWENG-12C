06550a2a3dba8eff6278732cbc0c4ad9cfda068b
kimileeee <kim_leejra@dlsu.edu.ph>
2023-02-01 21:56:15 +0800

    added enlist tests to check room capacity

--- src/test/java/com/orangeandbronze/enlistment/StudentTest.java
+++ src/test/java/com/orangeandbronze/enlistment/StudentTest.java
@@ -8,13 +8,14 @@ import static com.orangeandbronze.enlistment.Days.*;
 public class StudentTest {
 
     static final Schedule DEFAULT_SCHEDULE = new Schedule(MTH, H1000);
+    static final Room DEFAULT_ROOM = new Room("A1706", 45);
     @Test
     void enlist_2_sections_no_conflict() { // happy scenario
                     // pseudocode //
         // Given 1 student and 2 sections w/ no conflict
         Student student = new Student(1);
-        Section sec1 = new Section("A", DEFAULT_SCHEDULE);
-        Section sec2 = new Section("B", new Schedule(MTH, H0830));
+        Section sec1 = new Section("A", DEFAULT_SCHEDULE, DEFAULT_ROOM);
+        Section sec2 = new Section("B", new Schedule(MTH, H0830), DEFAULT_ROOM);
         // When student enlists in both sections
         student.enlist(sec1);
         student.enlist(sec2);
@@ -29,11 +30,42 @@ public class StudentTest {
     void enlist_2_sections_same_schedule() { // negative scenario
         // Given a student and 2 sections with same schedule
         Student student = new Student(1);
-        Section sec1 = new Section("A", DEFAULT_SCHEDULE);
-        Section sec2 = new Section("B", DEFAULT_SCHEDULE);
+        Section sec1 = new Section("A", DEFAULT_SCHEDULE, DEFAULT_ROOM);
+        Section sec2 = new Section("B", DEFAULT_SCHEDULE, DEFAULT_ROOM);
         // When student enlists in both sections
         student.enlist(sec1);
         // Then on the 2nd enlistment an exception should be thrown
         assertThrows(ScheduleConflictException.class, () -> student.enlist(sec2));
+    }
+
+    @Test
+    void enlist_section_not_at_room_capacity() { // positive scenario
+        // Given 2 students and a section with a room capacity of 1
+        Student student1 = new Student(1);
+        Student student2 = new Student(2);
+        Section sec = new Section("A", DEFAULT_SCHEDULE, DEFAULT_ROOM);
+        // When student1 enlists in the section
+        student1.enlist(sec);
+        // Then on student2 enlists in the same section, both students should be enlisted in the section and sectionEnlistment should be 2
+        student2.enlist(sec);
+        Collection<Section> sections1 = student1.getSections();
+        Collection<Section> sections2 = student2.getSections();
+        assertAll(
+                () -> assertTrue(sections1.contains(sec)),
+                () -> assertTrue(sections2.contains(sec)),
+                () -> assertEquals(2, sec.getSectionEnlistment())
+        );
+    }
+
+    @Test
+    void enlist_section_at_room_capacity() { // negative scenario
+        // Given 2 students and a section with a room capacity of 1
+        Student student1 = new Student(1);
+        Student student2 = new Student(2);
+        Section sec = new Section("A", DEFAULT_SCHEDULE, new Room("A1705", 1));
+        // When student1 enlists in the section
+        student1.enlist(sec);
+        // Then on student2 enlists in the same section, an exception should be thrown
+        assertThrows(RuntimeException.class, () -> student2.enlist(sec));
     }
 }
