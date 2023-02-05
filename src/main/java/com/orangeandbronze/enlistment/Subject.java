0240f51eec8b51b40bbb1c463e9f89a748cf251c
kimileeee <kim_leejra@dlsu.edu.ph>
2023-02-05 14:07:31 +0800

    Create Subject.java

---
+++ src/main/java/com/orangeandbronze/enlistment/Subject.java
@@ -0,0 +1,43 @@
+package com.orangeandbronze.enlistment;
+
+import java.util.Objects;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.Validate;
+
+class Subject {
+    private final String subjectId;
+    private final int units;
+    private final boolean isLab;
+
+    Subject (String subjectId, int units, boolean isLab) {
+        Validate.notNull(subjectId);
+        Validate.isTrue(StringUtils.isAlphanumeric(subjectId), "Room Name must be AlphaNumeric, was " + roomName);
+        Validate.notNull(units);
+        Validate.notNull(isLab);
+
+        this.subjectId = subjectId;
+        this.units = units;
+        this.isLab = isLab;
+    }
+
+    @Override
+    public String toString() {
+        return subjectId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Subject subject = (Subject) o;
+
+        return Objects.equals(subjectId, subject.subjectId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(subjectId);
+    }
+}
